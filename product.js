55dfa40906157c59752fcb734b8b465f5c3f9b5f
Emilie Rose <embi0003@stud.kea.dk>
2025-01-30 12:05:49 +0100

    på vej

--- product.js
+++ product.js
@@ -1,18 +1,16 @@
 let productId = 1531;
 let productContainer = document.querySelector(".productContainer");
 
-fetch(`https:/kea-alt.del.dk/t7/api/products/§{productID}`)
+fetch(`https:/kea-alt.del.dk/t7/api/products/${productId}`)
   .then((response) => response.json())
-
   .then((data) => {
     productContainer.innerHTML = `
-
         <div class="product-image">
-          <img src="https://kea-alt-del.dk/t7/images/webp/640/§{productId}.webp" alt="Puma-tee" />
+          <img src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="Puma-tee" />
         </div>
 
         <div class="product-info">
-          <h2>Grey Solid Round Neck T-Shirt</h2>
+          <h2>${data.productdisplayname}</h2>
           <p><strong>Brand:</strong> Puma</p>
           <p><strong>Color:</strong> Grey</p>
           <p><strong>Inventory number:</strong> 1531</p>
